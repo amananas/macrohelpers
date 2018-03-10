978eff38630e6222a9a65b75fa6734cffbde2738
Alexandre Besnard <alexandre.besnard0@gmail.com>
2018-03-10 18:14:52 +0100

    Setting correct packaging behavior

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='macrohelpers',
+      version='1.0',
+      description='Macro utility classes and methods',
+      url='https://github.com/amananas/macrohelpers',
+      author='Amananas',
+      author_email='amananas.github@gmail.com',
+      license='MIT',
+      packages=['mh_inout'],
+      zip_safe=False)
